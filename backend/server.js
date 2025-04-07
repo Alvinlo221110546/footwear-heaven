83771fb5eaba9a3c68c121e03dfc730f66c352e9
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2025-04-07 20:09:36 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors"
 import path from 'path';
-import routers from './routes/index.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import cookieParser from 'cookie-parser';
 import routes from './routes/index.js';
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,19 +14,14 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8001;
 
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
-app.use(routers);
-
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true,
 }));
-
 app.use(cookieParser());
-
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -39,7 +32,6 @@ app.use((req, res, next) => {
 
 app.use('/api', routes);
 
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
