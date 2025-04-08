bd9577b76ba5440ea177df436e415e7d8a26f5fa
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2025-04-08 10:13:40 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
