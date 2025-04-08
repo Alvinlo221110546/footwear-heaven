3162c275d9db50f42e9daef8366709482e8df70d
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2025-04-08 12:01:24 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -17,18 +17,23 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
 app.use(cookieParser());
 
+
+const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000';
+
+app.use(cors({
+  origin: corsOrigin,
+  credentials: true,
+}));
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', corsOrigin);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
+
 
 app.use('/api', routes);
 
